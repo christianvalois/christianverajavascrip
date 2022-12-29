40e3ccc6ea4740a04c24b7ab6e730bc067e123dd
christianvera <christianvera74@hotmail.com>
2022-12-30 00:47:09 +0100

    modifique texto y ciclo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const autos = prompt ("ingresa la marca de tu auto");
 const modelo = prompt ("ingresa el modelo");
-alert("para el modelo de tu auto" + " " + autos + " " + modelo + " " + "tenemos dos modelos racing y sport en 16 y 17'");
+alert("para el modelo de tu auto" + " " + autos + " " + modelo + " " + "tenemos dos modelos de llantas, racing y sport en 16 y 17'");
 
 const diametro = prompt("Ingresar pulgadas");
 const diametroParseada = parseInt(diametro);
@@ -9,12 +9,11 @@ if (diametroParseada > 15 && diametroParseada < 17) {
   alert("este es el modelo racing y su precio es de $ 30000");
 } else if (diametroParseada === 17) {
   alert("este es el modelo sport y su precio es de $ 25000");
-} else if (diametroParseada > 17) {
+} else if (diametroParseada > 17 || 1) {
   alert("Entrada invalida")
 }
-for (let i = 1; i <= 1; i++) {
-    alert("para mas informacion llame al numero de contacto gracias");
-}
-for (let i = 1; i <=1; i++) {
-  alert("para una nueva busqueda recargar la pagina");
+for (let i = 1; i <= 2; i++) {
+    alert("para mas informacion llame al numero 55555");
+    alert("gracias por su visita");
+break;
 }
