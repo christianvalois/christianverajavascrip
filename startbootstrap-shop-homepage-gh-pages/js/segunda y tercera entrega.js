2d384ee193c7a5473cd32c88a6b67963bcdf3800
christianvera <christianvera74@hotmail.com>
2023-02-17 10:45:40 +0100

    modificand el formulario

--- startbootstrap-shop-homepage-gh-pages/js/segunda y tercera entrega.js
+++ startbootstrap-shop-homepage-gh-pages/js/segunda y tercera entrega.js
@@ -1,30 +1,30 @@
 class Neumaticos {
-    constructor({marca, medidas, precio,}) {
-      this.marca = marca;
-      this.medidas = medidas;
-      this.precio = precio;
-    }
+  constructor({ marca, medidas, precio, }) {
+    this.marca = marca;
+    this.medidas = medidas;
+    this.precio = precio;
   }
-  
-  const NeumaticosSport = new Neumaticos({
-    marca: "good year",
-    medidas: "215-60-R16",
-    precio: "iva incluido $ 40.000"
-    
-  });
-  
-  const NeumaticosMixtos = new Neumaticos({
+}
+
+const NeumaticosSport = new Neumaticos({
+  marca: "good year",
+  medidas: "215-60-R16",
+  precio: "iva incluido $ 40.000"
+
+});
+
+const NeumaticosMixtos = new Neumaticos({
   marca: "fate",
   medidas: "165-70-R14",
   precio: "iva incluido $30.000"
-  
-  });
-  
-  const NeumaticosNormales = new Neumaticos({
+
+});
+
+const NeumaticosNormales = new Neumaticos({
   marca: "firestone",
   medidas: "170-65-R14",
   precio: " iva incluido $25.000"
-  });
+});
 const neumaticosTacos = {
   ...NeumaticosMixtos,
   marca: "michelin"
@@ -33,19 +33,23 @@ const neumaticosPerfilBajo = {
   ...NeumaticosSport,
   marca: "Hankook"
 }
-  
-  let NeumaticosEconomicos = ["falken", "devica",];
-  NeumaticosEconomicos.push("lanvigator","egommerce","kormoran");
- 
-  
-  let NeumaticosUltimasUnidadesFabricadas = ["traxmax", "orium", "uniroyal"];
-  NeumaticosUltimasUnidadesFabricadas.pop();
-  
-  
-  const ventaHastaAgotarStoc = NeumaticosEconomicos.concat(NeumaticosUltimasUnidadesFabricadas);
- 
-  
-  const ultimosNeumaticosEnVenta = [
+
+console.log(NeumaticosSport);
+console.log(NeumaticosMixtos);
+console.log(NeumaticosNormales);
+
+let NeumaticosEconomicos = ["falken", "devica",];
+NeumaticosEconomicos.push("lanvigator", "egommerce", "kormoran");
+console.log(NeumaticosEconomicos);
+
+let NeumaticosUltimasUnidadesFabricadas = ["traxmax", "orium", "uniroyal"];
+NeumaticosUltimasUnidadesFabricadas.pop();
+console.log(NeumaticosUltimasUnidadesFabricadas);
+
+const ventaHastaAgotarStoc = NeumaticosEconomicos.concat(NeumaticosUltimasUnidadesFabricadas);
+console.log(ventaHastaAgotarStoc);
+
+const ultimosNeumaticosEnVenta = [
   {
     marca: "falken",
     medidas: "170-75-16",
@@ -66,22 +70,32 @@ const neumaticosPerfilBajo = {
     medidas: "150-60-14",
     precio: " oferta sin iva $8.000 "
   },
-  ];
-localStorage.setItem("marcasDiscontinuas", JSON.stringify( ultimosNeumaticosEnVenta));
-    const listado = document.createElement("p");
-    for (const marcas of ultimosNeumaticosEnVenta) {
-      const p = document.createElement("p");
-      p.innerHTML = `${marcas.marca}`;
-      listado.append(p);
-    }
- document.querySelector("#pocotiempo").append(listado);
+];
+localStorage.setItem("marcasDiscontinuas", JSON.stringify(ultimosNeumaticosEnVenta));
+const UltimosNeumaticosGuardadosEnLe = localStorage.getItem("marcasDiscontinuas");
+ JSON.parse(UltimosNeumaticosGuardadosEnLe); 
+const listado = document.createElement("p");
+for (const marcas of ultimosNeumaticosEnVenta) {
+  const p = document.createElement("p");
+  p.innerHTML = `${marcas.marca}`;
+  listado.append(p);
+}
+document.querySelector("#pocotiempo").append(listado);
+const formulario = document.getElementById("formulario");
+let boton = document.get
 
- const formulario = document.getElementById("formulario");
  formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
- e.target[0].value;
- e.target[1].value;
- for(const input of e.target) {
+ e.preventDefault();
+ const ingresoFormulario = {
+ nombre: e.target[0].value,
+ email: e.target[1].value,
+}
+for(const input of e.target) {
   input.value = "";
- }
- });
+}
+localStorage.setItem("datos usuario", JSON.stringify(ingresoFormulario));
+boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+});
+});
+
